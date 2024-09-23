e35993674cbe3743b5cb41701ade7a051e430851
Demetrius Redmond <demetriusredmondofl@outlook.com>
2024-09-23 08:49:59 -0500

    tested use of lambda

--- calculator.py
+++ calculator.py
@@ -6,17 +6,13 @@ def getMathematicalOperation():
     operator = input("Enter operator: ")
     return operator
 
-def add(num1, num2):
-    return num1 + num2
+add = lambda num1, num2: num1 + num2
+    
+subtract = lambda num1, num2: num1 - num2
 
-def subtract(num1, num2):
-    return num1 - num2
+multiply = lambda num1, num2: num1 * num2
 
-def multiply(num1, num2):
-    return num1 * num2
-
-def divide(num1, num2):
-    return num1 / num2
+divide = lambda num1, num2: num1 + num2
 
 def calculate():
 
